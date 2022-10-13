485df70f2324e87279f6032b8d619d1f24d04aac
hojun-jo <86751964+hojun-jo@users.noreply.github.com>
2022-10-13 13:27:16 +0900

    install ejs and create main.css

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const helmet = require("helmet");
 const app = express();
+const ejs = require("ejs");
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use("/public", express.static(__dirname + "/public"));
 
 app.use(helmet());
+// post body에서 받기 위해 필요
+app.use(express.json());
+app.use(express.urlencoded());
+// post body에서 받기 위해 필요
 
 const mainRouter = require("./router/mainRouter");
 app.use("/", mainRouter);
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res) {
-  res.send("Hello World");
+  res.render("index", { title: "EJS 메인 페이지" });
 });
 
 router.get("/about", function (req, res) {
   res.send("About Page");
+});
+
+router.post("/postapi", function (req, res) {
+  let body = req.body;
+  console.log(body);
+  res.send("POST API");
 });
 
 module.exports = router;
